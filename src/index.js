be3c70fa6e033a91600e714d216267f230960c8a
Sepi70 <ghaffarisepi@gmail.comecho trigger run  trigger.txt>
2026-03-28 23:21:42 +1100

    fix all eslint issues

--- src/index.js
+++ src/index.js
@@ -1,28 +1,168 @@
-case 0: {
-    let flightId = "";
-    let flightIndex = -1;
+import readlineSync from "readline-sync";
+import { generateFlightId, isValidDateString, logSeparated, logWrapped, wrapString } from "./utilities.js";
+import { createFlightEntry, printScheduleTable } from "./flightScheduleDisplay.js";
+
+const lineLength = 94;
+const mainMenuOptions = ["View current flight schedule", "Log flight change", "EXIT"];
+const scheduleChangeMenuOptions = ["Change existing flight date", "Add new flight"];
+
+let airlines = ["Qantas", "Jetstar", "Virgin"];
+let flights = [
+    {id: "QA187", airline: "Qantas", origin: "Sydney", destination: "Perth", date: "15/05/2024"},
+    {id: "JE095", airline: "Jetstar", origin: "Gold Coast", destination: "Alice Springs", date: "07/06/2024"},
+    {id: "VI783", airline: "Virgin", origin: "Bangkok", destination: "London", date: "16/08/2024"},
+];
+
+let input = "";
+
+do {
+    console.clear();
+    logSeparated("MENU", lineLength);
+    input = readlineSync.keyInSelect(mainMenuOptions, "Please select an action to continue", {cancel: false});
+
+    switch (input) {
+        case 0:
+            console.clear();
+            logSeparated("Current Schedule", lineLength);
+            printScheduleTable(flights);
+            readlineSync.keyInPause(wrapString("Press q to return to main menu..."), {limit: ["q"], guide: false});
+            console.clear();
+            break;
+
+        case 1: {
+            console.clear();
+            logSeparated("Update Schedule", lineLength);
+            const choice = readlineSync.keyInSelect(scheduleChangeMenuOptions, "Please select an action to continue");
+            console.clear();
+
+            switch (choice) {
+                case 0: {
+                    logSeparated("Change Flight Date", lineLength);
+
+                    let flightId = "";
+                    let flightIndex = -1;
+
+                    do {
+                        flightId = readlineSync.question(wrapString("Enter the id of the flight to change the date for: "));
+                        for (let i = 0; i < flights.length; i++) {
+                            if (flights[i].id.toLowerCase() === flightId.toLowerCase()) {
+                                flightIndex = i;
+                                break;
+                            }
+                        }
+
+                        if (flightIndex < 0) {
+                            logWrapped(`ERROR: Flight ID ${flightId} not found.`);
+                        }
+
+                    } while (flightIndex < 0);
+
+                    logWrapped(`The current departure date for ${flights[flightIndex].id} is ${flights[flightIndex].date}`);
+
+                    const date = enterFlightDate();
+                    flights[flightIndex].date = date;
+
+                    logWrapped(`Flight successfully updated!`);
+                    readlineSync.keyInPause(wrapString("Press q to return to main menu..."), {limit: ["q"], guide: false});
+                    break;
+                }
+
+                case 1: {
+                    console.clear();
+                    logSeparated("Add New Flight", lineLength);
+
+                    let airlineIndex = 0;
+                    let cachedLength = 0;
+
+                    do {
+                        airlineIndex = readlineSync.keyInSelect([...airlines, "Add New Airline"], "Select an existing airline or add a new one ", {cancel: false});
+                        cachedLength = airlines.length;
+
+                        if (airlineIndex === airlines.length) {
+                            let isValid = false;
+                            let oldLength = airlines.length;
+
+                            do {
+                                airlines = addAirline(readlineSync.question(wrapString("Enter the name of the airline to add: ")), airlines);
+                                isValid = oldLength !== airlines.length;
+                            } while (!isValid);
+                        }
+
+                    } while (airlineIndex === cachedLength);
+
+                    const origin = readlineSync.question(wrapString("Enter the location the flight will depart from: "));
+                    const destination = readlineSync.question(wrapString("Enter the destination of the flight: "));
+                    const date = enterFlightDate();
+
+                    const flight = {
+                        id: generateFlightId(airlines[airlineIndex]),
+                        airline: airlines[airlineIndex],
+                        origin,
+                        destination,
+                        date
+                    };
+
+                    flights.push(flight);
+
+                    logWrapped(`Successfully added flight ${flight.id} with the following details:`);
+                    console.log(createFlightEntry(flight));
+
+                    readlineSync.keyInPause(wrapString("Press q to return to main menu..."), {limit: ["q"], guide: false});
+                    break;
+                }
+
+                case 2:
+                    break;
+            }
+
+            console.clear();
+            break;
+        }
+
+        case 2:
+            break;
+    }
+
+} while (input !== 2);
+
+console.clear();
+logWrapped("EXITING...", lineLength);
+
+function enterFlightDate() {
+    let date = "";
+    let isValid = false;
 
     do {
-        flightId = readlineSync.question(wrapString("Enter the id of the flight to change the date for: "));
-        for (let i = 0; i < flights.length; i++) {
-            if (flights[i].id.toLowerCase() === flightId.toLowerCase()) {
-                flightIndex = i;
-                break;
-            }
+        date = readlineSync.question(wrapString("Enter the departure date of this flight using the format DD/MM/YYYY: "));
+        isValid = isValidDateString(date);
+
+        if (!isValid) {
+            logWrapped("ERROR: The provided date does not use the correct format or is not a real date.");
         }
 
-        if (flightIndex < 0) {
-            logWrapped(`ERROR: Flight ID ${flightId} not found.`);
+    } while (!isValid);
+
+    return date;
+}
+
+function addAirline(airline, airlines) {
+    if (airline.trim() === "") {
+        logWrapped("ERROR: Airline name cannot be blank.");
+        return airlines;
+    }
+
+    let existing = false;
+
+    airlines.forEach(a => {
+        if (a === airline) {
+            logWrapped("ERROR: Airline already exists.");
+            existing = true;
         }
+    });
 
-    } while (flightIndex < 0);
+    if (existing) return airlines;
 
-    logWrapped(`The current departure date for ${flights[flightIndex].id} is ${flights[flightIndex].date}`);
-
-    const date = enterFlightDate();
-    flights[flightIndex].date = date;
-
-    logWrapped(`Flight successfully updated!`);
-    readlineSync.keyInPause(wrapString("Press q to return to main menu..."), {limit: ["q"], guide: false});
-    break;
+    airlines.push(airline);
+    logWrapped(`Airline ${airline} successfully added.`);
+    return airlines;
 }
